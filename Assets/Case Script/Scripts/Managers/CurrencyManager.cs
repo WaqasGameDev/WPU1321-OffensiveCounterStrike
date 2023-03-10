47b84ee2d755a8710c9e7243e58853285f201bc6
Aqil Noor <aqilnoor.wyrtual.io@gmail.com>
2023-03-10 23:59:02 +0500

    iap and login ui fixed

--- Assets/Case Script/Scripts/Managers/CurrencyManager.cs
+++ Assets/Case Script/Scripts/Managers/CurrencyManager.cs
@@ -2,10 +2,10 @@
 //Copyright © 2016-2018 Polygon Planet. All rights reserved. https://polygonplanet.com/privacy-policy/
 //This source file is subject to Unity Technologies Asset Store Terms of Service. https://unity3d.com/legal/as_terms
 
-//#pragma warning disable 0168 //Variable declared, but not used.
-//#pragma warning disable 0219 //Variable assigned, but not used.
-//#pragma warning disable 0414 //Private field assigned, but not used.
-/*#pragma warning disable 0649*/ //Variable asisgned to, and will always have default value.
+#pragma warning disable 0168 //Variable declared, but not used.
+#pragma warning disable 0219 //Variable assigned, but not used.
+#pragma warning disable 0414 //Private field assigned, but not used.
+#pragma warning disable 0649 //Variable asisgned to, and will always have default value.
 
 using System;
 using UnityEngine;
